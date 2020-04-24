6c75022d4897ca43c7ab338ffb47cb4571d77e7b
Rahul Butani <rr.butani@gmail.com>
2020-04-24 15:05:26 -0500

    bindings: some progress

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+mod xterm;
+
+#[wasm_bindgen]
+pub fn foo(x: &str) -> String {
+  if x == "abc" {
+    "yes".to_string()
+  } else {
+    "no".to_string()
+  }
+}
+
