31ae96294cca568860a44f31121f13768026f458
Rahul Butani <rr.butani@gmail.com>
2020-04-26 13:49:23 -0500

    examples: update basic to listen for key events

--- examples/basic/src/lib.rs
+++ examples/basic/src/lib.rs
@@ -8,6 +8,8 @@ use console_error_panic_hook::set_once as set_panic_hook;
 use wasm_bindgen::prelude::*;
 use xterm_js_sys::xterm::Terminal;
 
+macro_rules! log { ($($t:tt)*) => {web_sys::console::log_1(&format!($($t)*).into())}; }
+
 #[wasm_bindgen]
 pub fn run() -> Result<(), JsValue> {
     set_panic_hook();
@@ -19,7 +21,35 @@ pub fn run() -> Result<(), JsValue> {
     let terminal_div = document.get_element_by_id("terminal").expect("should have a terminal div");
 
     let term = Terminal::new(None);
+
+    // Make it 'static:
+    // Box::leak(Box::new(term));
+
     term.open(terminal_div);
+
+    let term_copy = term.clone();
+    let l = term.attach_key_event_listener(move |e| {
+        term_copy.write("a".to_string());
+
+        log!("yooo {:?}", e);
+    });
+
+    // Don't drop!
+    Box::leak(Box::new(l));
+
+    term.focus();
+
+    term.write(String::from("\x1B[35;31m hello\n"));
+    term.write(String::from("\x1B[1;3;31mxterm.js\x1B[0m $ "));
+
+    // Closure::wrap(Box::new() as Box)
+
+    // window.request_animation_frame()
+
+    // loop {
+    //     log!("hello!");
+
+    // }
 
     Ok(())
 }
