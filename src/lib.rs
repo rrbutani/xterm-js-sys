08ce2477aca2c01979611a19f0acd1accc3e2418
Rahul Butani <rr.butani@gmail.com>
2020-05-16 17:14:01 -0500

    misc: add some todos

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 #![cfg_attr(docs, feature(doc_cfg))]
 #![cfg_attr(docs, feature(external_doc))]
 
-extern crate wasm_bindgen;
+
+// TODO:
+//  - colours for badges in the README
+//  - deny the warnings here
+//  - add in an example/crate level docs here
+//  - add in the attr for marking feature specific things in docs
 
 pub mod xterm;
 
