7bc7e9110280b71a5467846f048c115738825c89
Rahul Butani <rr.butani@gmail.com>
2020-05-25 18:36:17 -0700

    ext: first pass at a Disposable trait

--- src/ext/disposable.rs
+++ src/ext/disposable.rs
@@ -2,10 +2,62 @@
 //!
 //! [`Disposable`]: crate::xterm::Disposable
 
-use super::{wasm_bindgen, Disposable};
-use wasm_bindgen::{JsCast, JsValue};
+use super::{object, Disposable, Terminal, TerminalOptions};
 
+use wasm_bindgen::{prelude::{wasm_bindgen, Closure}, JsCast, JsValue};
 use js_sys::{Function, Object};
+
+use core::ops::{Deref, DerefMut};
+
+/// This is the Rust version of the [`Disposable`](super::Disposable) interface.
+///
+/// See the ["mirroring interfaces" section](../../xterm#mirroring-interfaces)
+/// of the `xterm` module docs for more information.
+#[cfg_attr(docs, doc(cfg(feature = "ext")))]
+pub trait XtermDisposable {
+    fn dispose(&self);
+
+    // fn into_js(&self) -> Disposable where Self: Clone + 'static {
+    //     let b = Box::leak(Box::new(self.clone()));
+    //     b.into_js_inner().unchecked_into()
+    // }
+
+    fn into_js(self) -> Disposable where Self: 'static {
+        let b = Box::leak(Box::new(self);
+        b.into_js_inner().unchecked_into()
+    }
+
+    #[doc(hidden)]
+    fn into_js_inner(&'static self) -> Object where Self: 'static {
+        let disp: Box<dyn FnMut(JsValue)> = Box::new(|_s| Self::dispose(self));
+        let disp = Closure::wrap(disp);
+        Closure::forget(disp);
+
+        object!({
+            dispose: disp
+        })
+    }
+}
+
+/// In the `wasm-bindgen` world, things that impl an interface or extend a class
+/// `Deref` into it (technically, I think they only `Deref` into their immediate
+/// parent and then impl `AsRef` for all the other things they implement).
+///
+/// We've chosen to represent the `Disposable` interface with a corresponding
+/// Rust trait and this blanket impl implements the trait for all things that
+/// 'implement' the interface the `wasm-bindgen` way.
+///
+/// See the ["mirroring interfaces" section](../../xterm#mirroring-interfaces)
+/// of the `xterm` module docs for more information.
+impl<D: AsRef<Disposable> + Clone + 'static> XtermDisposable for D {
+    fn dispose(&self) {
+        Disposable::dispose(self.as_ref())
+    }
+
+    fn into_js(&self) -> Disposable {
+        self.as_ref().clone()
+    }
+}
 
 /// A wrapper for [`Disposable`] that calls `dispose` on `Drop`.
 #[derive(Debug, Clone)]
--- src/ext/mod.rs
+++ src/ext/mod.rs
@@ -1,8 +1,37 @@
 //! Glue for the Xterm.js types.
 
-use super::xterm::Disposable;
+use super::xterm::{Disposable, Terminal, TerminalOptions};
 
-use wasm_bindgen::prelude::*;
+/// Defines a JS object with some properties.
+#[macro_export]
+macro_rules! object {
+    ({
+        $($f:ident: $v:expr),* $(,)?
+    }) => {{
+
+        let obj = js_sys::Object::new();
+
+        object! {
+            obj += {
+                $($f: $v),*
+            }
+        }
+
+        obj
+    }};
+
+    ($nom:ident += {
+        $($f:ident: $v:expr),* $(,)?
+    }) => {$(
+        let $nom = js_sys::Object::define_property(
+            &$nom,
+            &wasm_bindgen::JsValue::from_str(core::stringify!($f)),
+            ($v).as_ref(),
+        );
+    )*};
+}
+
+use super::object;
 
 pub mod disposable;
 pub use disposable::*;
