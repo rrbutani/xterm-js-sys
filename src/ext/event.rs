17dcf9bfb7ed11689864bb6b1a929c8cb550bb45
Rahul Butani <rr.butani@gmail.com>
2020-06-02 13:28:31 -0700

    ext: clean up the friendly event functions + add some new ones

--- src/ext/event.rs
+++ src/ext/event.rs
@@ -1,7 +1,7 @@
 //! Sugar for methods that take `IEvent`s.
 
 use super::{Disposable, DisposableWrapper};
-use crate::xterm::{KeyEventData, Terminal};
+use crate::xterm::{KeyEventData, Terminal, BufferNamespace, Buffer};
 
 use wasm_bindgen::prelude::*;
 
@@ -31,11 +31,10 @@ impl Terminal {
     ///
     /// This is sugar for [`Terminal::on_binary`].
     ///
-    /// We assume event listener closures are going to be long-lived, so we
-    /// leak the closure that is produced here!
+    /// We assume event listener closures are going to be long-lived, so we leak
+    /// the closure that is produced here!
     ///
     /// [`Terminal::on_binary`]: Terminal::on_binary
-    #[allow(trivial_casts)]
     pub fn attach_binary_event_listener<F>(
         &self,
         listener: F,
@@ -44,13 +43,39 @@ impl Terminal {
         F: FnMut(String),
         F: 'static,
     {
-        let listener =
-            Closure::wrap(Box::new(listener) as Box<dyn FnMut(String)>);
+        let listener: Box<dyn FnMut(String)> = Box::new(listener);
+        let listener = Closure::wrap(listener);
+
         let ret = self.on_binary(&listener).into();
 
         Closure::forget(listener);
         ret
     }
+
+    /// Attaches a cursor move event listener and returns a
+    /// [`DisposableWrapper`] that can be dropped to make xterm.js stop sending
+    /// the event listener events.
+    ///
+    /// This is sugar for [`Terminal::on_cursor_move`].
+    ///
+    /// [`Terminal::on_cursor_move`]: Terminal::on_cursor_move
+    pub fn attach_cursor_move_event_listener<F>(
+        &self,
+        listener: F,
+    ) -> DisposableWrapper<Disposable>
+    where
+        F: FnMut(),
+        F: 'static,
+    {
+        let listener: Box<dyn FnMut()> = Box::new(listener);
+        let listener = Closure::wrap(listener);
+
+        let ret = self.on_cursor_move(&listener).into();
+
+        Closure::forget(listener);
+        ret
+    }
+
 
     /// Attaches a key event listener and returns a [`DisposableWrapper`]
     /// that can be dropped to make xterm.js stop sending the event listener
@@ -58,11 +83,10 @@ impl Terminal {
     ///
     /// This is sugar for [`Terminal::on_key`].
     ///
-    /// We assume event listener closures are going to be long-lived, so we
-    /// leak the closure that is produced here!
+    /// We assume event listener closures are going to be long-lived, so we leak
+    /// the closure that is produced here!
     ///
     /// [`Terminal::on_key`]: Terminal::on_key
-    #[allow(trivial_casts)]
     pub fn attach_key_event_listener<F>(
         &self,
         listener: F,
@@ -71,11 +95,43 @@ impl Terminal {
         F: FnMut(KeyEventData),
         F: 'static,
     {
-        let listener =
-            Closure::wrap(Box::new(listener) as Box<dyn FnMut(KeyEventData)>);
+        let listener: Box<dyn FnMut(KeyEventData)> = Box::new(listener);
+        let listener = Closure::wrap(listener);
+
         let ret = self.on_key(&listener).into();
 
         Closure::forget(listener);
         ret
     }
+}
+
+#[cfg_attr(all(docs, not(doctest)), doc(cfg(feature = "ext")))]
+impl BufferNamespace {
+    /// Attaches an event listener for when the active buffer changes and
+    /// returns a [`DisposableWrapper`] that can be dropped to make xterm.js
+    /// stop sending the event listener events.
+    ///
+    /// This is sugar for [`BufferNamespace::on_buffer_change`].
+    ///
+    /// We assume event listener closures are going to be long-lived, so we leak
+    /// the closure that is produced here!
+    ///
+    /// [`BufferNamespace::on_buffer_change`]: BufferNamespace::on_buffer_change
+    pub fn attach_buffer_change_event_listener<F>(
+        &self,
+        listener: F,
+    ) -> DisposableWrapper<Disposable>
+    where
+        F: FnMut(Buffer),
+        F: 'static,
+    {
+        let listener: Box<dyn FnMut(Buffer)> = Box::new(listener);
+        let listener = Closure::wrap(listener);
+
+        let ret = self.on_buffer_change(&listener).into();
+
+        Closure::forget(listener);
+        ret
+    }
+
 }
