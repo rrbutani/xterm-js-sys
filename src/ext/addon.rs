e3adfe063bb6844b761613ea7e8f0e223cb3c2f1
Rahul Butani <rr.butani@gmail.com>
2020-06-01 22:50:20 -0700

    ext: use the interface macro for Disposable and Addon

--- src/ext/addon.rs
+++ src/ext/addon.rs
@@ -2,124 +2,18 @@
 //!
 //! [Addons]: crate::xterm::TerminalAddon
 
-use super::{object, Disposable, IntoJsInterface, TerminalAddon, Terminal};
+use super::{interface, Disposable, IntoJsInterface, TerminalAddon, Terminal};
 use super::disposable::XtermDisposable;
 
-use js_sys::Object;
-use wasm_bindgen::{
-    prelude::Closure,
-    JsCast,
-};
-
-/// This is the Rust version of the [`TerminalAddon`] interface.
-///
-/// See the ["mirroring interfaces" section](../../xterm#mirroring-interfaces)
-/// of the `xterm` module docs for more information.
-///
-/// [`TerminalAddon`]: crate::xterm::TerminalAddon
-#[allow(clippy::module_name_repetitions)]
-pub trait XtermAddon: XtermDisposable {
-    /// This is called when the addon is activated. Dual of
-    /// [`TerminalAddon::activate`].
-    ///
-    /// [`TerminalAddon::activate`]: TerminalAddon::activate
-    fn activate(&self, terminal: Terminal);
-
-    //////////////// Internal Functions For Interface Mirroring ////////////////
-
-    /// Copy of [`IntoJsInterface::by_ref`].
-    ///
-    /// [`IntoJsInterface::by_ref`]: IntoJsInterface::by_ref
-    fn into_js_by_ref(&self) -> TerminalAddon
+interface! {
+    #[cfg_attr(all(docs, not(doctest)), doc(cfg(feature = "ext")))]
+    #[allow(clippy::module_name_repetitions)]
+    pub trait XtermAddon mirrors TerminalAddon
     where
-        Self: Clone + 'static,
+        Self extends Disposable as XtermDisposable,
     {
-        XtermAddon::into_js(self.clone())
-    }
-
-    /// Copy of [`IntoJsInterface::to`].
-    ///
-    /// [`IntoJsInterface::to`]: IntoJsInterface::to
-    fn into_js(self) -> TerminalAddon
-    where
-        Self: Sized + 'static,
-    {
-        let b = Box::leak(Box::new(self));
-        XtermAddon::into_js_inner(b).unchecked_into()
-    }
-
-    /// Internal version of `into_js_by_ref` that doesn't leak `self`.
-    ///
-    /// Useful for trait hierarchies.
-    fn into_js_inner(&'static self) -> Object
-    where
-        Self: 'static,
-    {
-        let act: Box<dyn FnMut(_)> =
-            Box::new(move |t| Self::activate(self, t));
-        let act = Closure::wrap(act);
-
-        let obj = object! { (<Self as XtermDisposable>::into_js_inner(self)) += {
-            activate: act
-        }};
-
-        Closure::forget(act);
-
-        obj
-    }
-}
-
-/// Anything that implements [`XtermAddon`] (and is `Clone + 'static`)
-/// implements [`IntoJsInterface<TerminalAddon>`][`IntoJsInterface`].
-impl<A> IntoJsInterface<TerminalAddon> for A
-where
-    A: XtermAddon + Clone + 'static,
-{
-    /// Converts the [`XtermAddon`] implementor into an instance of
-    /// [`TerminalAddon`] (the corresponding JS interface).
-    fn to(self) -> TerminalAddon {
-        XtermAddon::into_js(self)
-    }
-
-    /// Converts the [`XtermAddon`] implementor into an instance of
-    /// [`TerminalAddon`] (the corresponding JS interface) _by reference_.
-    fn by_ref(&self) -> TerminalAddon {
-        XtermAddon::into_js_by_ref(self)
-    }
-}
-
-/// This provides an impl of the [`XtermAddon`] Rust trait for all things that
-/// 'implement' the [`TerminalAddon`] JS interface the `wasm-bindgen` way.
-///
-/// See the ["mirroring interfaces" section](../../xterm#mirroring-interfaces)
-/// of the `xterm` module docs for more information.
-impl<A: Clone + 'static> XtermAddon for A
-where
-    A: AsRef<TerminalAddon>,
-    A: AsRef<Disposable>,
-{
-    /// `activate` for types that implement the [`TerminalAddon`] interface.
-    fn activate(&self, terminal: Terminal) {
-        TerminalAddon::activate(self.as_ref(), terminal)
-    }
-
-    /// `into_js_by_ref` for types that implement the [`TerminalAddon`]
-    /// interface.
-    ///
-    /// This differs from the default impl in that in manages to avoid a `Clone`
-    /// before effectively doing what `into_js` does.
-    fn into_js_by_ref(&self) -> TerminalAddon {
-        AsRef::<TerminalAddon>::as_ref(self).clone()
-    }
-
-    /// `into_js` for types that implement the [`TerminalAddon`] interface.
-    ///
-    /// This differs from the default impl in that in manages to avoid "double
-    /// wrapping" the methods in the interface (types that impl
-    /// [`TerminalAddon`] the `wasm-bindgen` way already have an wrapped up
-    /// [`Object`] they can hand us).
-    fn into_js(self) -> TerminalAddon {
-        AsRef::<TerminalAddon>::as_ref(&self).clone()
+        /// This is called when the addon is activated.
+        fn activate(&self, terminal: Terminal);
     }
 }
 
--- src/ext/disposable.rs
+++ src/ext/disposable.rs
@@ -6,119 +6,22 @@ use super::{object, Disposable, IntoJsInterface, Terminal, TerminalOptions};
 
 use js_sys::{Function, Object};
 use wasm_bindgen::{
-    prelude::{wasm_bindgen, Closure},
+    prelude::wasm_bindgen,
     JsCast,
 };
 
 use core::ops::{Deref, DerefMut};
 
-/// This is the Rust version of the [`Disposable`](super::Disposable) interface.
-///
-/// See the ["mirroring interfaces" section](../../xterm#mirroring-interfaces)
-/// of the `xterm` module docs for more information.
-#[cfg_attr(all(docs, not(doctest)), doc(cfg(feature = "ext")))]
-#[allow(clippy::module_name_repetitions)]
-pub trait XtermDisposable {
-    /// Disposes of the instance. Dual of [`Disposable::dispose`].
-    ///
-    /// This can involve unregistering an event listener or cleaning up
-    /// resources or anything else that should happen when an instance is
-    /// disposed of.
-    fn dispose(&self);
-
-    //////////////// Internal Functions For Interface Mirroring ////////////////
-
-    /// Copy of [`IntoJsInterface::by_ref`].
-    ///
-    /// [`IntoJsInterface::by_ref`]: IntoJsInterface::by_ref
-    fn into_js_by_ref(&self) -> Disposable
-    where
-        Self: Clone + 'static,
-    {
-        self.clone().into_js()
-    }
-
-    /// Copy of [`IntoJsInterface::to`].
-    ///
-    /// [`IntoJsInterface::to`]: IntoJsInterface::to
-    fn into_js(self) -> Disposable
-    where
-        Self: Sized + 'static,
-    {
-        let b = Box::leak(Box::new(self));
-        b.into_js_inner().unchecked_into()
-    }
-
-    /// Internal version of `into_js_by_ref` that doesn't leak `self`.
-    ///
-    /// Useful for trait hierarchies.
-    fn into_js_inner(&'static self) -> Object
-    where
-        Self: 'static,
-    {
-        let disp: Box<dyn FnMut()> =
-            Box::new(move || Self::dispose(self));
-        let disp = Closure::wrap(disp);
-
-        let obj = object! { dispose: disp };
-
-        Closure::forget(disp);
-
-        obj
-    }
-}
-
-/// Anything that implements [`XtermDisposable`] (and is `Clone + 'static`)
-/// implements [`IntoJsInterface<Disposable>`][`IntoJsInterface`].
-impl<D> IntoJsInterface<Disposable> for D
-where
-    D: XtermDisposable + Clone + 'static,
-{
-    /// Converts the [`XtermDisposable`] implementor into an instance of
-    /// [`Disposable`] (the corresponding JS interface).
-    fn to(self) -> Disposable {
-        self.into_js()
-    }
-
-    /// Converts the [`XtermDisposable`] implementor into an instance of
-    /// [`Disposable`] (the corresponding JS interface) _by reference_.
-    fn by_ref(&self) -> Disposable {
-        self.into_js_by_ref()
-    }
-}
-
-/// In the `wasm-bindgen` world, things that impl an interface or extend a class
-/// `Deref` into it (technically, I think they only `Deref` into their immediate
-/// parent and then impl `AsRef` for all the other things they implement).
-///
-/// We've chosen to represent the [`Disposable`] interface with a corresponding
-/// Rust trait and this blanket impl implements the trait for all things that
-/// 'implement' the interface the `wasm-bindgen` way.
-///
-/// See the ["mirroring interfaces" section](../../xterm#mirroring-interfaces)
-/// of the `xterm` module docs for more information.
-impl<D: AsRef<Disposable> + Clone + 'static> XtermDisposable for D {
-    /// `dispose` for types that implement the [`Disposable`] interface.
-    fn dispose(&self) {
-        Disposable::dispose(self.as_ref())
-    }
-
-    /// `into_js_by_ref` for types that implement the [`Disposable`] interface.
-    ///
-    /// This differs from the default impl in that in manages to avoid a `Clone`
-    /// before effectively doing what `into_js` does.
-    fn into_js_by_ref(&self) -> Disposable {
-        AsRef::<Disposable>::as_ref(self).clone()
-    }
-
-    /// `into_js` for types that implement the [`Disposable`] interface.
-    ///
-    /// This differs from the default impl in that in manages to avoid "double
-    /// wrapping" the methods in the interface (types that impl [`Disposable`]
-    /// the `wasm-bindgen` way already have an wrapped up [`Object`] they can
-    /// hand us).
-    fn into_js(self) -> Disposable {
-        AsRef::<Disposable>::as_ref(&self).clone()
+interface! {
+    #[cfg_attr(all(docs, not(doctest)), doc(cfg(feature = "ext")))]
+    #[allow(clippy::module_name_repetitions)]
+    pub trait XtermDisposable mirrors Disposable {
+        /// Disposes of the instance.
+        ///
+        /// This can involve unregistering an event listener or cleaning up
+        /// resources or anything else that should happen when an instance is
+        /// disposed of.
+        fn dispose(&self);
     }
 }
 
